4ff958379b320b095a57180350aa2f9bcf27f823
PIUS <relimoringa@gmail.com>
2023-06-22 12:56:01 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
-}
+    var input = document.getElementById('input');
+    var eventListenerBound = false;
+  
+    function handleInputChange() {
+      eventListenerBound = true;
+    }
+  
+    // Add event listener
+    input.addEventListener('change', handleInputChange);
+  
+    // Return whether the event listener is bound or not
+    return eventListenerBound;
+  }
+  
